870fa3fc22fecff93d16c1758eb8248ec9783c11
sib_dip <tskdip@gmail.com>
2023-03-02 13:00:41 +0600

    First commit
    -init online_employees_catalog project
    -add settings
    -init employees_catalog_app

---
+++ online_employees_catalog/setenv.sh
@@ -0,0 +1,7 @@
+export DJANGO_SECRET_KEY='6l=@e2!@zngdz9ry!7&(l-*28y=zy47q4--s^#vuh2c2qr)t*b'
+export DJANGO_DEBUG=True
+export DJANGO_DB_NAME='employees_catalog_db'
+export DJANGO_DB_USER='db_user'
+export DJANGO_DB_PASS='user_pass'
+export DJANGO_DB_HOST='127.0.0.1'
+export DJANGO_DB_PORT='5432'
