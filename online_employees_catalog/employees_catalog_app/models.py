00c918b714d9c546a83b4ef3f85f6fc10cae9571
sib_dip <tskdip@gmail.com>
2023-03-04 15:55:52 +0600

    Add post_save chief check for Empoloyee model

--- online_employees_catalog/employees_catalog_app/models.py
+++ online_employees_catalog/employees_catalog_app/models.py
@@ -1,13 +1,26 @@
 from django.db import models
+from django.db.models.signals import post_save
+
 
 class Employee(models.Model):
     """Model for employees information"""
-    name = models.CharField(max_length=255)
-    role = models.CharField(max_length=255)
+    name = models.CharField('Имя', max_length=255)
+    role = models.CharField('Должность', max_length=255)
     # TODO add positive salary validator
-    salary = models.FloatField()
-    chief = models.ForeignKey("self", on_delete=models.SET_NULL, null=True)
+    salary = models.FloatField('Заработная плата')
     employment_date = models.DateField('Дата приёма на работу')
+    chief = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
+def employee_post_save_chief_check(sender, instance, *args, **kwargs):
+    """Make an employee chief for himself if None is chosen when create"""
+    if instance.chief is None:
+        self_chief = instance
+        self_chief.chief = instance
+        self_chief.save()
 
+
+post_save.connect(employee_post_save_chief_check, sender=Employee)
