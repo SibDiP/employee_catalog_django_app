0e9751ed8cb7483a6f250acf97a0512d5bf87bf4
sib_dip <tskdip@gmail.com>
2023-03-05 19:35:35 +0600

    Hide hierarchy lvl attribute in Employee creation form

--- online_employees_catalog/employees_catalog_app/admin.py
+++ online_employees_catalog/employees_catalog_app/admin.py
@@ -1,9 +1,22 @@
+from django import forms
 from django.contrib import admin
 from .models import Employee
+
+class EmployeeAdminForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Hide the 'my_field' field in the instance creation form
+        self.fields['hierarchy_lvl_counter'].widget = forms.HiddenInput()
 
 
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = 'name', 'role', 'chief', 'salary', 'employment_date'
+    form = EmployeeAdminForm
+
 
 admin.site.register(Employee, EmployeeAdmin)
 
