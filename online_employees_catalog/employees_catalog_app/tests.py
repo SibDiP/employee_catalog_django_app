0c8a9fb6bc132b0c0bce2bb80b086d3ca9d8eb7a
sib_dip <tskdip@gmail.com>
2023-03-05 19:36:50 +0600

    Add Employee model creation test

--- online_employees_catalog/employees_catalog_app/tests.py
+++ online_employees_catalog/employees_catalog_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Employee
 
-# Create your tests here.
+
+class EmployeeCreateTestCase(TestCase):
+    def setUp(self):
+        self.chief_employee = Employee.objects.create(
+            name='Джо Денжер',
+            role='Глава отдела тестирования',
+            salary=300000,
+            employment_date='2023-01-01'
+        )
+
+    def test_employee_creation(self):
+        self.assertEqual(self.chief_employee.name, 'Джо Денжер')
+        self.assertEqual(self.chief_employee.role, 'Глава отдела тестирования')
+        self.assertEqual(self.chief_employee.salary, 300000)
+        self.assertEqual(self.chief_employee.employment_date, '2023-01-01')
