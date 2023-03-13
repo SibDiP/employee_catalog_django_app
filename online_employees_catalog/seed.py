e47f07c6c43e76a83108be1de402f3e32443eba1
sib_dip <tskdip@gmail.com>
2023-03-13 14:34:26 +0600

    Imporve and Refactor seed.py
    1. Refactor employee creation:
    	before: calculate cheif for every employee creation
    	now: calculate one chief for SEED_EMPLOYEE_PER_CHIEF_AMOUNT (50 so far)
    2. Refactor main script logic:
    	before: one loop which creates one employee at iteration
    	now: there is constants SEED_EMPLOYEE_AMOUNT, SEED_EMPLOYEE_PER_CHIEF_AMOUNT which are used to calculate seed_iteration_amount and
    		seed_iteration_remainder_amount. So, main loop now itearte seed_iteration_amount times and than create whats left by using
    		seed_iteration_remainder_amount.
    3. Add a unic names for every employee with names_generator lib
    4. Add a normalised salary randomiser
    5. Add a simple role template based on hierarchy_lvl

--- online_employees_catalog/seed.py
+++ online_employees_catalog/seed.py
@@ -1,26 +1,52 @@
-import random
+"""Fulfill DB with fake employees
+Run from shell
+"""
+
 import django
 django.setup()
 from django_seed import Seed
 from employees_catalog_app.models import Employee
+import random
+from names_generator import generate_name
+
+# This three constants can be changed as needed
+SEED_EMPLOYEE_AMOUNT = 999
+SEED_EMPLOYEE_PER_CHIEF_AMOUNT = 50
+SEED_HIERARCHY_LVL_MAX_DEEP = 5
+
+seed_iteration_amount = SEED_EMPLOYEE_AMOUNT // SEED_EMPLOYEE_PER_CHIEF_AMOUNT
+seed_iteration_remainder_amount = SEED_EMPLOYEE_AMOUNT % SEED_EMPLOYEE_PER_CHIEF_AMOUNT
 
 
-def custom_chief_provider():
-    # Get all employees with a hierarchy_lvl_counter less than 5
-    employees = Employee.objects.filter(hierarchy_lvl_counter__lt=5)
-    # Select a random employee from the list
+def custom_chief_provider(max_hierarchy_lvl) -> object:
+    """
+    :return: random employee with hierarchy_lvl less than 5
+    """
+    employees = Employee.objects.filter(hierarchy_lvl_counter__lt=max_hierarchy_lvl)
+
     if employees.exists():
         return random.choice(employees)
     else:
         return None
 
 
-seeder = Seed.seeder()
+def employee_create(employee_per_chief: int, max_hierarchy_lvl: int) -> None:
+    seeder = Seed.seeder()
+    chief = custom_chief_provider(max_hierarchy_lvl)
+    employee_hierarchy_lvl = chief.hierarchy_lvl_counter + 1
 
-# Register the custom provider for the 'chief' field
-for i in range(1,49900):
-    seeder.add_entity(Employee, 1, {
-        'chief': custom_chief_provider()
-    })
+    for _ in range(employee_per_chief):
+        seeder.add_entity(Employee, 1, {
+            'name': generate_name(style='capital'),
+            'role': f"Employee lvl {employee_hierarchy_lvl}",
+            'salary': round(random.random()*1000),
+            'chief': chief
+        })
     seeder.execute()
+    print(f'{employee_per_chief} employees added')
 
+
+for i in range(seed_iteration_amount):
+    employee_create(SEED_EMPLOYEE_PER_CHIEF_AMOUNT, SEED_HIERARCHY_LVL_MAX_DEEP)
+
+employee_create(seed_iteration_remainder_amount, SEED_HIERARCHY_LVL_MAX_DEEP)
