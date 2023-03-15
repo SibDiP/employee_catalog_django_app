04e220ff553763f457024291113ecbd8870de3b3
sib_dip <tskdip@gmail.com>
2023-03-15 19:27:41 +0600

    Upgrade employee_catalog views.py and urls.py for catalog tree page

--- online_employees_catalog/employee_catalog/views.py
+++ online_employees_catalog/employee_catalog/views.py
@@ -5,4 +5,4 @@ from .models import Employee
 def employee_tree(request):
     employees = Employee.objects.all()
     context = {'employees': employees}
-    return render(request, 'employee_tree.html', context)
+    return render(request, 'employee_catalog/employee_catalog_tree.html', context)
