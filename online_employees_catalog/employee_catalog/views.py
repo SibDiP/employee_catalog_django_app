ea24c215638ef9ef30a5b39df86d1bd3c7f13e8d
sib_dip <tskdip@gmail.com>
2023-03-17 13:35:35 +0600

    Fix and revork of tree display [1] 	before: tree was duplicate instances 	now: it display only lvl_1 employess 	Idea: split display to two functions, and two templates with recursion: 		1 - for general tree (loop through lvl_1 employees 		2 - for loop through QuerySet of subordinates

--- online_employees_catalog/employee_catalog/views.py
+++ online_employees_catalog/employee_catalog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
+from django.shortcuts import render, get_object_or_404
 from .models import Employee
 
 
-def employee_tree(request):
-    employees = Employee.objects.all()
-    context = {'employees': employees}
+# by hierarchy lvl 1
+def employee_tree(request) -> QuerySet:
+    employees = Employee.objects.filter(hierarchy_lvl_counter=1)
+    context = {'lvl_1_employees': employees}
+    print(type(employees))
     return render(request, 'employee_catalog/employee_catalog_tree.html', context)
+
+
+# by objects_list ->
+def specific_employee_tree(request, pk):
+    pass
+    # employee = get_object_or_404(Employee, pk=pk)
+    # employees = employee.employee_set.all
+    # context = {'employees': employees}
+    #
+    # return render(request, 'employee_catalog/employee_catalog_tree.html', context)
