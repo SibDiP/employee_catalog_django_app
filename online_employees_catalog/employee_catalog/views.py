9e6cf06802adfed8a342a465b5dea715bb1980ac
sib_dip <tskdip@gmail.com>
2023-03-23 16:11:01 +0600

    Update view.py and templates/employee_catalog/employee_catalog_tree.html to new model

--- online_employees_catalog/employee_catalog/views.py
+++ online_employees_catalog/employee_catalog/views.py
@@ -1,21 +1,6 @@
-from django.db.models import Max
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from .models import Employee
 
-
 def employee_tree(request):
-    max_hierarchy_lvl_counter = Employee.objects.aggregate(Max('hierarchy_lvl_counter'))['hierarchy_lvl_counter__max']
-    employees = Employee.objects.filter(hierarchy_lvl_counter=1)
-    context = {'lvl_1_employees': employees, 'max_hierarchy_lvl_counter': max_hierarchy_lvl_counter}
-    return render(request, 'employee_catalog/employee_catalog_tree.html', context)
-
-
-
-# by objects_list ->
-def specific_employee_tree(request, pk):
-    pass
-    # employee = get_object_or_404(Employee, pk=pk)
-    # employees = employee.employee_set.all
-    # context = {'employees': employees}
-    #
-    # return render(request, 'employee_catalog/employee_catalog_tree.html', context)
+    root_nodes = Employee.get_root_nodes()
+    return render(request, 'employee_catalog/employee_catalog_tree.html', {'root_nodes': root_nodes})
