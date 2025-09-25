4c0741e86f0b381d0e368cf6e0bfe28d443a3d17
Dmitrii Pivnev <tskdip@gmail.com>
2025-09-25 20:08:39 +0700

    Moved from setenv.sh to python-decouple and .env

--- online_employees_catalog/online_employees_catalog/settings.py
+++ online_employees_catalog/online_employees_catalog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG')
+DEBUG = config('DEBUG', default="FALSE", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -80,11 +81,11 @@ WSGI_APPLICATION = 'online_employees_catalog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     os.environ.get('DJANGO_DB_NAME'),
-        'USER':     os.environ.get('DJANGO_DB_USER'),
-        'PASSWORD': os.environ.get('DJANGO_DB_PASS'),
-        'HOST':     os.environ.get('DJANGO_DB_HOST'),
-        'PORT':     os.environ.get('DJANGO_DB_PORT'),
+        'NAME':     config('DB_NAME'),
+        'USER':     config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST':     config('DB_HOST'),
+        'PORT':     config('DB_PORT'),
     }
 }
 
