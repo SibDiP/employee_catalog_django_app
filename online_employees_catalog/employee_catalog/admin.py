45e6cd6b69f84bac9c4ed0019719ed20b0d736d9
Dmitrii Pivnev <tskdip@gmail.com>
2025-10-20 18:12:48 +0700

    style(admin): change EmployeeAdmin title

--- online_employees_catalog/employee_catalog/admin.py
+++ online_employees_catalog/employee_catalog/admin.py
@@ -8,5 +8,10 @@ from .models import Employee
 class EmployeeAdmin(TreeAdmin):
     form = movenodeform_factory(Employee)
 
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['title'] = "Управление сотрудниками"
+        return super().changelist_view(request, extra_context=extra_context)
+
 
 admin.site.register(Employee, EmployeeAdmin)
